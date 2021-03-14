abee03352f0277cd9f636c222b1fdb0377f920e7
pjdip <pjdip1@gmail.com>
2021-03-14 17:22:38 -0400

    projects husk created

---
+++ src/pages/Projects/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+const Projects = () => {
+    return (
+        <main class="container bg-white mt-5 mx-auto">
+            <div class="row pt-4 text-center text-success">
+                <div class="col">
+                    <h2>Projects</h2>
+                </div>
+            </div>
+
+        </main>
+    );
+};
+
+export default Projects;
