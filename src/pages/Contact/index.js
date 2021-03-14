17f272305236a4e0c96ffab6309add07293a0111
pjdip <pjdip1@gmail.com>
2021-03-14 16:22:02 -0400

    working on contact page

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./style.css";
+
+const Contact = () => {
+    return (
+        <div className="contact row mt-3">
+            <div class="col">
+                <ul>
+                    <li>
+                        <p>Email: pjdip1@gmail.com</p>
+                    </li>
+                    <li>
+                        <p>Phone: 443-812-4170</p>
+                    </li>
+                    <li>
+                        <p>
+                            <a target="_blank" href="https://github.com/pjdip">
+                                <i class="fab fa-github">
+                                </i>
+                                Github
+                            </a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a target="_blank" href="https://www.linkedin.com/in/philip-pj-dipaula/">
+                                <i class="fab fa-linkedin">
+                                </i>
+                                LinkedIn
+                            </a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <a target="_blank" href="./assets/Resume_Philip_DiPaula.pdf">Resume.pdf</a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
