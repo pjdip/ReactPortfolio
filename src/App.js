e78321833981dd5e72db886f022a66e66e13442f
pjdip <pjdip1@gmail.com>
2021-03-14 17:18:23 -0400

    adding navbar using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navb from "./components/Navbar";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
@@ -9,6 +10,7 @@ function App() {
     return (
         <Router>
             <div>
+                <Navb />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/contact" component={Contact} />
                 <Footer />
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "./style.css";
+
+const Navb = () => {
+    return (
+        <Navbar bg="success" expand="sm">
+            <Navbar.Brand>Philip DiPaula</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default Navb;
