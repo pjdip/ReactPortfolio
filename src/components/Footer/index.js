2df58cde8100052fc515391494e5fc6d1ac6f52d
pjdip <pjdip1@gmail.com>
2021-03-14 15:49:53 -0400

    Footer Component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+    return (
+        <footer className="bg-success text-white text-center mt-4 p-3">
+            <h4>&copy; Made with ❤️️ by Philip DiPaula</h4>
+        </footer>
+    )
+};
+
+export default Footer;
