3ffeb8cd8019ac5aed2af83e882f79d009f3ff87
pjdip <pjdip1@gmail.com>
2021-03-16 17:29:55 -0400

    broswerrouter with basename

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbr from "./components/Navbar";
 import Container from "./components/Container";
 import About from "./pages/About";
@@ -11,7 +11,7 @@ import "./App.css";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div>
                 <Navbr />
                 <Container>
