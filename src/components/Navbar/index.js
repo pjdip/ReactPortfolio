220592f85b3e2fc94782cec746c24ba2f98aa8aa
pjdip <pjdip1@gmail.com>
2021-03-16 17:40:00 -0400

    adding link from react-router-dom to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import "./style.css";
 
@@ -9,10 +10,10 @@ const Navbr = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto mr-3">
-                    <Nav.Link className="text-white" href="/about">About</Nav.Link>
-                    <Nav.Link className="text-white" href="/gallery">Gallery</Nav.Link>
-                    <Nav.Link className="text-white" href="/projects">Projects</Nav.Link>
-                    <Nav.Link className="text-white" href="/contact">Contact</Nav.Link>
+                    <Nav.Link as={Link} className="text-white" href="/about">About</Nav.Link>
+                    <Nav.Link as={Link} className="text-white" href="/gallery">Gallery</Nav.Link>
+                    <Nav.Link as={Link} className="text-white" href="/projects">Projects</Nav.Link>
+                    <Nav.Link as={Link} className="text-white" href="/contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
